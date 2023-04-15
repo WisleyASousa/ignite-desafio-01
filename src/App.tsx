67d681b17a6a43c13686edec923fef43c4349ca1
WisleyASousa <wisleya.sousa@outlook.com.br>
2023-04-15 09:35:16 -0300

    finalizando componente informativo das tasks, iniciando body das tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Header } from './components/Header'
 import { NewItem } from './components/NewItem'
 import { Tasks } from './components/Tasks'
+import styles from './App.module.css'
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
-      <NewItem />
-      <Tasks />
+      <div className={styles.container}>
+        <NewItem />
+        <Tasks />
+      </div>
     </div>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -2,14 +2,17 @@ import styles from './Tasks.module.css';
 
 export function Tasks() {
   return (
-      <div className={styles.container}>
+      <div className={styles.containerTasks}>
         <div className={styles.infoContainer}>
           <div className={styles.infoTasks}>
             <p>Tarefas criadas</p><span>0</span>
           </div>
           <div className={styles.infoTasks}>
-            <p>Concluídas</p><span>0</span>
+            <p className={styles.tasksCompleted}>Concluídas</p><span>0</span>
           </div>
+        </div>
+        <div className={styles.boxTasks}>
+
         </div>
 
       </div>
